26a6b1ff8efa356678cb43b09154d90aac7ed18f
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-08-31 21:32:55 -0500

    fixed everything and java is now reading correctly in order.

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
 // ask if they want upper case -confirm
 // // ask if they want lower case -cornfirm
@@ -23,27 +23,29 @@ generateBtn.addEventListener("click", writePassword);
 // return completed password 
 
 function generatePassword() {
+    var availableChars = "";
+    var completedPassword = "";
   var wantUppy = confirm("Do you want to include uppercase character?")
   var wantLowy = confirm("Do you want to include lowercase characters?")
   var wantNumbies = confirm("Do you want to include numbers?")
   var specialChars = confirm("Do you want to include special characters")
   var passwordLength = prompt("How long do you want password to be? (8-128)")
-    if ((parsInt(passwordLength) <= 8) && (parseInt(passwordLength) >= 128)) {
+    if ((parseInt(passwordLength) >= 8) && (parseInt(passwordLength) <= 128)) {
     
     if (wantUppy) {
-      availabeChars = availabeChars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      availableChars = availableChars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     }
 
     if (wantLowy) {
-      availabeChars = availabeChars + "abcdefghijklmnopqrstuvwxyz"
+      availableChars = availableChars + "abcdefghijklmnopqrstuvwxyz"
     }
 
     if (wantNumbies) {
-      availabeChars = availabeChars + "1234567890"
+      availableChars = availableChars + "1234567890"
     }
 
     if (specialChars) {
-      availabeChars = availabeChars = "!@#$%^&*()"
+      availableChars = availableChars + "!@#$%^&*()"
     }
 
     for(var i = 0; i < parseInt(passwordLength); i++) {
@@ -51,10 +53,12 @@ function generatePassword() {
       var randomChars = availableChars[randomIndex];
       completedPassword = completedPassword + randomChars;
     }
+
+    return completedPassword;
+
   } else{ 
     alert("The length of your password needs to be at least 8 digits and at most 128.")
   }
-
-return completedPassword;
 }
-generatePassword()
+// generatePassword()
+generateBtn.addEventListener("click", writePassword);
